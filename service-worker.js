b321697c6a2be6a5ecbbca759b9be527ccd8933a
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-05-27 16:43:22 +0630

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "mka-portfolio-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./manifest.webmanifest",
+  "./assets/icon-192.png",
+  "./assets/icon-512.png",
+  "./assets/icon-maskable.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
